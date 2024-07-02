e7163862de7ccbec848498605b8ca1053cb71fdd
LLouis <llouis0622@gmail.com>
2024-07-03 04:41:27 +0900

    .

---
+++ Programmers/입문 문제/약수 구하기.py
@@ -0,0 +1,6 @@
+def solution(n):
+    ans = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            ans.append(i)
+    return ans
---
+++ Programmers/입문 문제/인덱스 바꾸기.py
@@ -0,0 +1,4 @@
+def solution(my_string, num1, num2):
+    lst = list(my_string)
+    lst[num1], lst[num2] = lst[num2], lst[num1]
+    return ''.join(lst)
