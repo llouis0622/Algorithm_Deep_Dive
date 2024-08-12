765cdc650a27c194499707065b05c84de9a11b79
LLouis <llouis0622@gmail.com>
2024-08-13 01:58:33 +0900

    .

---
+++ Programmers/Lv. 0/[PCCE] 01 출력.py
@@ -0,0 +1,7 @@
+string_msg = 'Spring is beginning'
+int_val = 3
+string_val = '3'
+
+print(string_msg)
+print(int_val)
+print(string_val)
---
+++ Programmers/Lv. 0/[PCCE] 02 피타고라스의 정리.py
@@ -0,0 +1,5 @@
+a = int(input())
+c = int(input())
+
+b_square = pow(c, 2) - pow(a, 2)
+print(b_square)
