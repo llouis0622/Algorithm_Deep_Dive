e0c0f0f0cbbe933cd02720d4dd5a9a3b35aff919
LLouis <llouis0622@gmail.com>
2024-08-17 00:21:50 +0900

    .

---
+++ Programmers/Lv. 0/[PCCE] 08 창고 정리.py
@@ -0,0 +1,13 @@
+def solution(storage, num):
+    clean_storage = []
+    clean_num = []
+    for i in range(len(storage)):
+        if storage[i] in clean_storage:
+            pos = clean_storage.index(storage[i])
+            clean_num[pos] += num[i]
+        else:
+            clean_storage.append(storage[i])
+            clean_num.append(num[i])
+    max_num = max(clean_num)
+    answer = clean_storage[clean_num.index(max_num)]
+    return answer
