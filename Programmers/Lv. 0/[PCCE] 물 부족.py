21a618163c1647592449b04b513bb451748f5e4b
LLouis <llouis0622@gmail.com>
2024-09-16 01:38:45 +0900

    .

---
+++ Programmers/Lv. 0/[PCCE] 물 부족.py
@@ -0,0 +1,8 @@
+def solution(storage, usage, change):
+    total_usage = 0
+    for i in range(len(change)):
+        usage = int(usage + usage * change[i] / 100)
+        total_usage += usage
+        if total_usage > storage:
+            return i
+    return -1
